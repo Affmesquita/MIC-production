edff02e3e6fe23cc8e4b6a8642d1795cbab495a6
Affonso Mesquita <102181682+Affmesquita@users.noreply.github.com>
2023-04-13 15:39:45 -0300

    atualizado

--- java-script/script.js
+++
@@ -1,9 +0,0 @@
-
-const btnMobile = document.getElementsByClassName('hamburger-menu');
-
-    function toggleMenu(){
-        const nav = document.getElementsByClassName('nav')
-        nav.classlist.toggle('active')
-    }
-
-btnMobile.addEventListener('click', toggleMenu);
